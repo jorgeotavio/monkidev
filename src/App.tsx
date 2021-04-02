41f11debb3e8b1c76b1d838c213e387c67871fbd
jorge <jorgelosb@gmail.com>
2021-04-02 16:12:31 -0300

    :rocket: commit inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled, { css } from "styled-components";
+import logo from "./assets/img/logo.png";
+
+interface Props {
+  selected: boolean;
+}
+
+const Card = styled.div<Props>`
+  font-family: "Roboto", sans-serif;
+  display: block;
+  margin: 5px 0;
+  background-color: white;
+  padding: 30px;
+  border-radius: 5px;
+  cursor: pointer;
+  border: 2px solid
+    ${(props: any) => (props.selected ? "var(--color-primary)" : "white")};
+  transition: border 0.4s;
+
+  .card-title {
+    font-family: "Roboto", sans-serif;
+    font-weight: 700;
+    color: var(--color-primary);
+  }
+
+  &:hover {
+    border-color: var(--color-primary);
+  }
+`;
+
+interface ButtonProps {
+  primary?: boolean;
+}
+
+const Button = styled.a<ButtonProps>`
+  display: block;
+  border-radius: 5px;
+  padding: 1rem 2rem;
+  margin-top: 5px;
+  color: white;
+  background: var(--color-primary);
+  font-family: "Roboto", sans-serif;
+  font-size: 18px;
+  text-decoration: none;
+  text-align: center;
+`;
 
 function App() {
+  const [planos] = useState([
+    {
+      id: 1,
+      name: "Básico",
+      price: 1200,
+    },
+    {
+      id: 2,
+      name: "médio",
+      price: 1330,
+    },
+    {
+      id: 3,
+      name: "Pro",
+      price: 1450,
+    },
+  ]);
+
+  const [selected, setSelected] = useState({ id: 0, name: "", price: 0 });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+    <div className="container">
+      <div className="main-wrapper">
+        <div className="logo-wrapper">
+          <div className="logo">
+            <img src={logo} />
+          </div>
+        </div>
+        <div className="title">Selecione um plano</div>
+        {planos.map((plan, idx) => (
+          <Card
+            onClick={() => setSelected(plan)}
+            selected={selected?.id === plan.id}
+            key={idx}
+          >
+            <div className="card-title">Plano {plan.name}</div>
+            <div>
+              {plan.price.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </div>
+          </Card>
+        ))}
+        <Button
           target="_blank"
-          rel="noopener noreferrer"
+          href={`https://wa.me/5587988589105?text=Olá eu gostaria de um site com o plano: \n ${selected.name}`}
+          primary={true}
         >
-          Learn React
-        </a>
-      </header>
+          Finalizar
+        </Button>
+      </div>
     </div>
   );
 }
