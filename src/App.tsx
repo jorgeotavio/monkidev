ba2cc73260c2cea1cd82d51dea6d32267804cf2a
you@example.com <you@example.com>
2021-04-10 11:21:34 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled, { css } from "styled-components";
 import logo from "./assets/img/logo.png";
+import { smoothScrollTo } from "./utils";
 
 interface Props {
   selected: boolean;
@@ -18,7 +19,34 @@ const Card = styled.div<Props>`
     ${(props: any) => (props.selected ? "var(--color-primary)" : "white")};
   transition: border 0.4s;
 
+  .checked {
+    position: relative;
+    margin-left: auto;
+    width: 15px;
+    height: 15px;
+    background-color: ${(props: any) =>
+      props.selected ? "var(--color-primary)" : "white"};
+    border: 2px solid var(--color-primary);
+    border-radius: 50%;
+  }
+
   .card-title {
+    font-size: 20px;
+    font-family: "Roboto", sans-serif;
+    font-weight: 700;
+    color: var(--color-primary);
+  }
+
+  .card-desc {
+    font-size: 18px;
+    font-family: "Roboto", sans-serif;
+    font-weight: 500;
+    color: var(--color-primary);
+    margin: 10px 0px;
+  }
+
+  .price {
+    font-size: 22px;
     font-family: "Roboto", sans-serif;
     font-weight: 700;
     color: var(--color-primary);
@@ -31,59 +59,85 @@ const Card = styled.div<Props>`
 
 interface ButtonProps {
   primary?: boolean;
+  disabled?: boolean;
 }
 
 const Button = styled.a<ButtonProps>`
   display: block;
   border-radius: 5px;
-  padding: 1rem 2rem;
-  margin-top: 5px;
+  padding: 2rem 2rem;
+  margin: 20px 0px;
   color: white;
-  background: var(--color-primary);
+  background: ${(props) =>
+    props.disabled ? "rgb(0,0,0,.2)" : "var(--color-primary)"};
   font-family: "Roboto", sans-serif;
   font-size: 18px;
   text-decoration: none;
   text-align: center;
+  cursor: ${(props) => props.disabled && "not-allowed"};
+  ${(props) =>
+    props.disabled &&
+    css({
+      pointerEvents: "none",
+    })}
 `;
 
 function App() {
   const [planos] = useState([
     {
       id: 1,
-      name: "Básico",
-      price: 1200,
+      name: "Landing Page para campanha",
+      desc: "Apenas 1 página",
+      price: 900,
     },
     {
       id: 2,
-      name: "médio",
-      price: 1330,
+      name: "Site Institucional Básico",
+      desc: "De 4 a 8 páginas",
+      price: 1199,
     },
     {
       id: 3,
-      name: "Pro",
-      price: 1450,
+      name: "Site Institucional Intermediário",
+      desc: "De 4 a 8 páginas",
+      price: 1499,
+    },
+    {
+      id: 4,
+      name: "Site Institucional Profissional",
+      desc: "Mais que 8 páginas, com mais recursos para operações complexas",
+      price: 2299,
     },
   ]);
 
   const [selected, setSelected] = useState({ id: 0, name: "", price: 0 });
+
+  const actionSetSelected = (plan: any) => {
+    let to = document.getElementById("finish")?.offsetTop;
+    to = to ? to : 0;
+    smoothScrollTo(0, to, 400);
+    setSelected(plan);
+  };
 
   return (
     <div className="container">
       <div className="main-wrapper">
         <div className="logo-wrapper">
           <div className="logo">
-            <img src={logo} />
+            <img src={logo} alt="logo" />
           </div>
         </div>
         <div className="title">Selecione um plano</div>
         {planos.map((plan, idx) => (
           <Card
-            onClick={() => setSelected(plan)}
+            onClick={() => actionSetSelected(plan)}
             selected={selected?.id === plan.id}
             key={idx}
           >
-            <div className="card-title">Plano {plan.name}</div>
-            <div>
+            <div className="checked"></div>
+            <div className="card-title">{plan.name}</div>
+            {plan.desc && <div className="card-desc">{plan.desc}</div>}
+            <div className="price">
               {plan.price.toLocaleString("pt-br", {
                 style: "currency",
                 currency: "BRL",
@@ -92,9 +146,11 @@ function App() {
           </Card>
         ))}
         <Button
+          id="finish"
           target="_blank"
           href={`https://wa.me/5587988589105?text=Olá eu gostaria de um site com o plano: \n ${selected.name}`}
           primary={true}
+          disabled={selected?.id === 0}
         >
           Finalizar
         </Button>
---
+++ src/utils.ts
@@ -0,0 +1,26 @@
+export function smoothScrollTo(endX: number, endY: number, duration: number) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== "undefined" ? duration : 400;
+
+  // Easing function
+  const easeInOutQuart = (time: number, from: number, distance: number, duration: number) => {
+    if ((time /= duration / 2) < 1)
+      return (distance / 2) * time * time * time * time + from;
+    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60); // 60 fps
+}
